0e7c3ce0581a6cb5fe44bc16bf6d50cd5a5daa15
hoangdatnguyen <54076449+datnh0710@users.noreply.github.com>
2025-12-09 10:00:25 -0600

    Upload source code Terraform for Docker

---
+++ terrafrom-docker/main.tf
@@ -0,0 +1,16 @@
+provider "docker" {}
+
+resource "docker_image" "nginx" {
+  name = "nginx:latest"
+  keep_locally = false
+}
+
+resource "docker_container" "nginx" {
+  name  = var.container_name
+  image = docker_image.nginx.image_id
+  ports {
+    internal = 80
+    external = 8000
+  }
+  
+}
